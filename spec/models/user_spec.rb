1b39ab8d86a9936bdefa08ffff6359fd3fc8bf7c
Cedric GOVAERTS <govaertscedric@gmail.com>
2026-05-04 20:13:46 +0200

    movies + test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,11 +11,16 @@ RSpec.describe User, type: :model do
   context "when invalid" do
     it "doesn't create the user when lacking most of items" do
       user = User.create(first_name: "cédric")
-      expect(user).to be_invalid
-    end
+      user_witout_email = User.create(first_name: "Cédric", last_name: "Govaerts", password: "123456")
+      user_without_last_name = User.create(first_name: "Cédric", email: "cedric@gmail.com", password: "123456")
+      user_without_password = User.create(first_name: "Cédric", last_name: "Govaerts", email: "cedric@gmail.com")
 
-    it "doesn't create the user when lacking email" do
-      user = User.create(first_name: "Cédric", last_name: "Govaerts", password: "123456")
+      expect(user).to be_invalid
+      expect(user_witout_email).to be_invalid
+      expect(user_without_last_name).to be_invalid
+      expect(user_without_password).to be_invalid
+
+      expect(User.count).to eq(0)
     end
   end
 end
