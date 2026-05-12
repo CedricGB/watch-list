90d8358cd82e0acd604479b95b18301db8743635
Cedric GOVAERTS <govaertscedric@gmail.com>
2026-05-12 09:09:10 +0200

    added columns to the list model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_04_195655) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_12_041952) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -26,7 +26,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_04_195655) do
 
   create_table "lists", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.datetime "ends_at"
     t.string "name"
+    t.datetime "starts_at"
     t.datetime "updated_at", null: false
   end
 
